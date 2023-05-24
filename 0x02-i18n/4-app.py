5c57467d26343e5d58ebba365c35aa9a06757372
Stephen Obialor <stevobialor@gmail.com>
2023-05-24 06:04:16 +0000

    done

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+"""
+This is a Flask app that displays a welcome message in the selected language.
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    welcome_title = gettext('home_title')
+    welcome_header = gettext('home_header')
+    return render_template('3-index.html', welcome_title=welcome_title, welcome_header=welcome_header)
+
+
+if __name__ == '__main__':
+    app.run()
